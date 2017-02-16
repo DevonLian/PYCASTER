231904b49933c6533602ac8dfbe5d820271843ec
Devon Lian <devleelooper@mail.com>
2017-02-16 19:54:05 +0000

    Start/Stop Script

---
+++ pycaster.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export PATH=$PATH:/usr/local/bin
+export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
+
+case "$1" in
+  start)
+  echo "Starting PYCASTER !"
+  forever start server.js
+  ;;
+stop)
+  echo "Stoping PYCASTER..."
+  exec forever stop server.js
+  ;;
+*)
+  echo "Usage: /etc/init.d/picast {start|stop}"
+  exit 1
+  ;;
+esac
+
